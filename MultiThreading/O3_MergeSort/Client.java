4d19e7b8672f50807000ce5f09e0f457d238421b
ABHISHEK KRISHNA <abhishekkrishna@ABHISHEKs-MacBook-Air.local>
2024-10-11 14:55:31 +0530

    Run Mergesort on different thread

--- MultiThreading/O3_MergeSort/Client.java
+++ MultiThreading/O3_MergeSort/Client.java
@@ -1,12 +1,22 @@
 package MultiThreading.O3_MergeSort;
 
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class Client {
     public static void main(String[] args) throws Exception{
         List<Integer> arr = List.of(6,4,3,8,2,1,10);
         MergeSorter mergeSorter = new MergeSorter(arr);
-        List<Integer> sortedData = mergeSorter.call();
+//        List<Integer> sortedData = mergeSorter.call();
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        Future<List<Integer>>  sortedDataFuture = executorService.submit(mergeSorter);
+
+        List<Integer> sortedData = sortedDataFuture.get(); // Blocking call waits of response
+
         System.out.println("Sorted array : "+sortedData);
+
+        executorService.shutdown();
     }
 }
